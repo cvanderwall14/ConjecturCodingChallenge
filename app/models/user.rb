063be326cbc598f8fcf904217af0facdeddf182c
Christian Vanderwall <cvanderwall14@gmail.com>
2014-12-11 22:33:24 +0000

    Add follow and unfollow buttons

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ActiveRecord::Base
     
     # Returns true if the current user is following the other user.
     def following?(other_user)
-    following.include?(other_user)
+        following.include?(other_user)
     end
 
 end
